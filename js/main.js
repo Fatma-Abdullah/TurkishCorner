1cbe30c57ae540a6b9b08773d0cb191dd939a864
unknown <fatmaabdallah78@yahoo.com>
2021-10-09 13:47:19 +0200

    latest

--- js/main.js
+++ js/main.js
@@ -86,6 +86,29 @@
 				}
 			}
 		});
+		$('.carousel-ShoppingCompanies').owlCarousel({
+			center: true,
+			loop: true,
+			items:1,
+			margin: 30,
+			stagePadding: 0,
+			nav: false,
+			autoplay: true,
+			animateOut: 'fadeOut',
+			animateIn: 'fadeIn',
+			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+			responsive:{
+				0:{
+					items: 1
+				},
+				600:{
+					items: 2
+				},
+				1000:{
+					items: 5
+				}
+			}
+		});
 	};
 	carousel();
 
